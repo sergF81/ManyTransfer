9e8490134b581179f546c850c9bad7770bbab9c7
Sergey Fortis <s_fortis@mail.ru>
2021-05-10 21:28:10 +0500

    Доработка - Денежный перевод на Котлин

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,25 +3,25 @@ import javax.print.attribute.IntegerSyntax
 import kotlin.math.roundToInt
 
 fun main() {
-    var comission: Int = 0;
+    var comission: Double = 0.0;
     println("________________________________________")
 
     while (true) {
 
-        println("Enter the amount of rubles you want to transfer. If you have finished translating, enter 0")
-        var amount = Integer.parseInt(readLine()) * 100;
-        if (amount == 0) {
+        println("Enter the amount of rubles you want to transfer. If you have finished translating, enter 0.0")
+        val amount = readLine()!!.toDouble() * 100;
+        if (amount == 0.0) {
             println("Transfer close!")
             break
         }
-        if (amount < 3500) {
-            println("The amount of the commission exceeds the transfer amount. Please enter a higher value for translation.")
+        if (amount < 0.0) {
+            println("The amount must be more than 0. Please enter a higher value for translation.")
             continue
         }
         if ((amount / 100) * 0.75 > 3500) {
-            comission = ((amount / 100) * 0.75).roundToInt()
-        } else comission = 3500
-        println("The commission for the transfer of " + amount / 100 + " rubles ($amount copeecs) is " + comission / 100 + " rubles ( $comission copeecs). Thank you for using the services of our service.")
+            comission = (amount / 100) * 0.75
+        } else comission = 3500.00
+        println("The commission for the transfer of " + String.format("%.2f", amount / 100) + " rubles (" + amount.toInt() + " copeecs) is " + String.format("%.2f", comission / 100) + " rubles (" + comission.toInt() + " copeecs). Thank you for using the services of our service.")
 
     }
 
