c1024bf72cd97095577d8afb4bb5e8d5334fefcc
Sergey Fortis <s_fortis@mail.ru>
2021-05-12 18:51:14 +0500

    Доработка 2 - Денежный перевод на Котлин

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,7 @@ fun main() {
 
         println("Enter the amount of rubles you want to transfer. If you have finished translating, enter 0.0")
         val amount = readLine()!!.toDouble() * 100;
+        comission = (amount / 100) * 0.75;
         if (amount == 0.0) {
             println("Transfer close!")
             break
@@ -18,12 +19,9 @@ fun main() {
             println("The amount must be more than 0. Please enter a higher value for translation.")
             continue
         }
-        if ((amount / 100) * 0.75 > 3500) {
-            comission = (amount / 100) * 0.75
-        } else comission = 3500.00
+        if (comission < 3500) {
+            comission = 3500.00
+        }
         println("The commission for the transfer of " + String.format("%.2f", amount / 100) + " rubles (" + amount.toInt() + " copeecs) is " + String.format("%.2f", comission / 100) + " rubles (" + comission.toInt() + " copeecs). Thank you for using the services of our service.")
-
     }
-
-
 }
